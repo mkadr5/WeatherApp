0eb49cd8c497738459833bda8a5ff7a2655ca6a2
mkadr5 <muhammetkadir52@gmail.com>
2023-04-09 23:53:37 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Muhammet Kadir on 9.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
